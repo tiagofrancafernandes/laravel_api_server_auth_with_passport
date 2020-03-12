540701827782e1228a1572c3c7a3bc94fc1c8389
França Tiago <tiago.fernandes@pontomais.com.br>
2020-03-12 15:11:29 -0300

    Atualizando o bash_aliases.sh de dev

--- _dev_dir/bash_alises.sh
+++ _dev_dir/bash_alises.sh
@@ -4,3 +4,9 @@ alias tinker='php ./artisan tinker'
 alias sqlite-local='sqlite3 --list $(pwd)/database/database.sqlite'
 
 PS1="\n___\n\w \$(date +%H-%M-%S)\n\u@\h \\$>\[$(tput sgr0)\]"
+
+alias artisan='php ./artisan'
+alias artisan-list='php ./artisan list'
+alias artisan-list-grep='php ./artisan list |grep'
+alias tinker='php ./artisan tinker'
+alias bashrc-update='source ~/.bashrc'
