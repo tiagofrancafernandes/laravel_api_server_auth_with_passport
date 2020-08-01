d99cb402021a3be6478d965b5a5592ca9dc15118
França Tiago <tiago.fernandes@pontomais.com.br>
2020-08-01 19:32:33 -0300

    Fix Middleware CorsControl

--- app/Http/Middleware/CorsControl.php
+++ app/Http/Middleware/CorsControl.php
@@ -15,14 +15,8 @@ class CorsControl
      */
     public function handle($request, Closure $next)
     {
-        $hosts = explode(";", env('CORS_ALLOW_HOSTS'));
-        foreach($hosts as $h)
-        {
-            if(strlen($h) > 5)
-            header("Access-Control-Allow-Origin : $h");
-        }
-        // header('Access-Control-Allow-Origin : *');
-        // header('Access-Control-Allow-Origin : *');
+        $host = env('CORS_ALLOW_HOST', '*');
+        header("Access-Control-Allow-Origin : $host");
         header('Access-Control-Allow-Headers : Content-type, X-Auth-Token, Authorization, Origin');
         return $next($request);
     }
