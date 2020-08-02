8fe6c0df37d5e3bcfaba852b4486a8f40e8a6e51
França Tiago <tiago.fernandes@pontomais.com.br>
2020-08-01 21:20:01 -0300

    ADD middleware to set languages

---
+++ app/Http/Middleware/Locale.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use App;
+use Config;
+use Session;
+
+class Locale
+{
+  /**
+   * Handle an incoming request.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @param  \Closure  $next
+   * @return mixed
+   */
+  public function handle($request, Closure $next)
+  {
+    //https://stackoverflow.com/a/42395646/11716408
+    $raw_locale = $request->session()->get('locale');
+    if (in_array($raw_locale, Config::get('app.locales')))
+    {
+      $locale = $raw_locale;
+    } else {
+      $locale = Config::get('app.locale');
+    }
+    App::setLocale($locale);
+    return $next($request);
+  }
+}
--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,11 @@ Auth::routes();//./vendor/laravel/ui/src/AuthRouteMethods.php (+- 19)
 Route::get('/logout', 'Auth\LoginController@logout')->name('web.auth.get.logout');
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('setlocale/{locale}', function ($locale) {
+    if (in_array($locale, \Config::get('app.locales')))
+    {
+        session(['locale' => $locale]);
+    }
+    return redirect()->back();
+})->name('setlocale');
